72f18c556473a5aff4218f71bfdd9385ecb435a4
akashdeep <akashdeep@shurutech.com>
2023-08-18 15:10:58 +0530

    Add v6,7,8,9 versions for the generated tests

--- src/test/java/saralarchan/org/core/CommonUtilsTest.java
+++ src/test/java/saralarchan/org/core/CommonUtilsTest.java
@@ -7,12 +7,16 @@ import org.mockito.Mockito;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.Cookie;
 import java.util.HashMap;
+import java.util.Map;
+
 import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
 import java.io.BufferedReader;
 import java.io.StringReader;
 
 public class CommonUtilsTest {
-
+    
     @Test
     public void testGetInputDataFromRequest() throws Exception {
         HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
@@ -22,14 +26,14 @@ public class CommonUtilsTest {
         String result = CommonUtils.getInputDataFromRequest(request);
         assertEquals(inputData, result);
     }
-
+    
     @Test
     public void testGetCorrelationID() {
         HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
         String result = CommonUtils.getCorrelationID(request);
         assertNotNull(result);
     }
-
+    
     @Test
     public void testGetCorrelationIDWithCookie() {
         HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
@@ -38,7 +42,7 @@ public class CommonUtilsTest {
         String result = CommonUtils.getCorrelationID(request);
         assertEquals("testID", result);
     }
-
+    
     @Test
     public void testResolvespecifier() {
         String token = "testToken";
@@ -48,7 +52,7 @@ public class CommonUtilsTest {
         String result = CommonUtils.resolvespecifier(token, ctx, respCtx, req);
         assertNotNull(result);
     }
-
+    
     @Test
     public void testSetTarget() {
         String target = "$VAR.testTarget";
@@ -58,7 +62,7 @@ public class CommonUtilsTest {
         boolean result = CommonUtils.setTarget(target, varmap, dc, value);
         assertTrue(result);
     }
-
+    
     @Test
     public void testResolveSource() {
         String value = "$VAR.testValue";
@@ -68,20 +72,20 @@ public class CommonUtilsTest {
         String result = CommonUtils.resolveSource(value, varmap, req);
         assertEquals("resolvedValue", result);
     }
-
+    
     @Test
     public void testGetGlobalProp() {
         String key = "java.version";
         String result = CommonUtils.getGlobalProp(key);
         assertNotNull(result);
     }
-
+    
     @Test
     public void testInitSystemProps() {
         boolean result = CommonUtils.initSystemProps();
         assertTrue(result);
     }
-
+    
     @Test
     public void testPrepareResponse() {
         HashMap ctx = new HashMap();
@@ -90,7 +94,7 @@ public class CommonUtilsTest {
         HashMap result = CommonUtils.prepareResponse(ctx, dc, rv);
         assertNotNull(result);
     }
-
+    
     @Test
     public void testGetCorrelationIDWithNoCookies() {
         HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
@@ -98,7 +102,7 @@ public class CommonUtilsTest {
         String result = CommonUtils.getCorrelationID(request);
         assertNotNull(result);
     }
-
+    
     @Test
     public void testResolvespecifierWithNoTokenParts() {
         String token = "CONTEXT";
@@ -108,7 +112,7 @@ public class CommonUtilsTest {
         String result = CommonUtils.resolvespecifier(token, ctx, respCtx, req);
         assertEquals(ctx.toString(), result);
     }
-
+    
     @Test
     public void testSetTargetWithNoVar() {
         String target = "$.testTarget";
@@ -118,16 +122,16 @@ public class CommonUtilsTest {
         boolean result = CommonUtils.setTarget(target, varmap, dc, value);
         assertTrue(result);
     }
-
+    
     // Removed testResolveSourceWithNoVar test as it was causing error
-
+    
     @Test
     public void testGetGlobalPropWithNoKey() {
         String key = "nonexistent.key";
         String result = CommonUtils.getGlobalProp(key);
         assertNull(result);
     }
-
+    
     @Test
     public void testPrepareResponseWithOverride() {
         HashMap ctx = new HashMap();
@@ -141,5 +145,67 @@ public class CommonUtilsTest {
         assertEquals("overrideResponse", result.get("RESPONSE"));
         assertEquals(200, result.get("STATUS_CODE"));
         assertEquals("overrideHeaders", result.get("RESPONSE_HEADERS"));
+    }
+    
+    //Added manually from v8 file
+    @Test
+    public void testResolvespecifierWithContext() {
+        HashMap<String, String> ctx = new HashMap<>();
+        ctx.put("key", "value");
+        DocumentContext respCtx = JsonPath.parse("{}");
+        String result = CommonUtils.resolvespecifier("CONTEXT", ctx, respCtx, new Object());
+        assertEquals("{key=value}", result);
+    }
+    
+    @Test
+    public void testResolvespecifierWithDC() {
+        HashMap<String, String> ctx = new HashMap<>();
+        DocumentContext respCtx = JsonPath.parse("{\"key\":\"value\"}");
+        String result = CommonUtils.resolvespecifier("DC", ctx, respCtx, new Object());
+        assertEquals("{\"key\":\"value\"}", result);
+    }
+    
+    @Test
+    public void testResolvespecifierWithRequest() {
+        HashMap<String, String> ctx = new HashMap<>();
+        DocumentContext respCtx = JsonPath.parse("{}");
+        String result = CommonUtils.resolvespecifier("REQUEST", ctx, respCtx, "{\"key\":\"value\"}");
+        assertEquals("{\"key\":\"value\"}", result);
+    }
+    
+    @Test
+    public void testResolvespecifierWithVar() {
+        HashMap<String, String> ctx = new HashMap<>();
+        ctx.put("key", "value");
+        DocumentContext respCtx = JsonPath.parse("{}");
+        String result = CommonUtils.resolvespecifier("$VAR.key", ctx, respCtx, new Object());
+        assertEquals("value", result);
+    }
+    
+    @Test
+    public void testResolvespecifierWithRequestJsonPath() {
+        HashMap<String, String> ctx = new HashMap<>();
+        DocumentContext respCtx = JsonPath.parse("{}");
+        Map<String, String> req = new HashMap<>();
+        req.put("key", "value");
+        String result = CommonUtils.resolvespecifier("$.key", ctx, respCtx, req);
+        assertEquals("value", result);
+    }
+    
+    @Test
+    public void testResolvespecifierWithSystem() {
+        HashMap<String, String> ctx = new HashMap<>();
+        DocumentContext respCtx = JsonPath.parse("{}");
+        System.setProperty("key", "value");
+        String result = CommonUtils.resolvespecifier("$SYSTEM.key", ctx, respCtx, new Object());
+        assertEquals("value", result);
+    }
+    
+    @Test
+    public void testResolvespecifierWithInvalidToken() {
+        HashMap<String, String> ctx = new HashMap<>();
+        DocumentContext respCtx = JsonPath.parse("{}");
+        String result = CommonUtils.resolvespecifier("INVALID", ctx, respCtx, new Object());
+        assertEquals("INVALID", result);
     }
 }
