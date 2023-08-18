f0112f28ec465f53abcc3df015349bda887ac612
akashdeep <akashdeep@shurutech.com>
2023-08-18 13:45:12 +0530

    Common Utils tests working after 3 generations

---
+++ src/test/java/saralarchan/org/core/CommonUtilsTest.java
@@ -0,0 +1,93 @@
+
+package saralarchan.org.core;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mockito;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Cookie;
+import java.util.HashMap;
+import com.jayway.jsonpath.DocumentContext;
+import java.io.BufferedReader;
+import java.io.StringReader;
+
+public class CommonUtilsTest {
+
+    @Test
+    public void testGetInputDataFromRequest() throws Exception {
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        String inputData = "test input data";
+        BufferedReader reader = new BufferedReader(new StringReader(inputData));
+        Mockito.when(request.getReader()).thenReturn(reader);
+        String result = CommonUtils.getInputDataFromRequest(request);
+        assertEquals(inputData, result);
+    }
+
+    @Test
+    public void testGetCorrelationID() {
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        String result = CommonUtils.getCorrelationID(request);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testGetCorrelationIDWithCookie() {
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        Cookie[] cookies = {new Cookie("correlationID", "testID")};
+        Mockito.when(request.getCookies()).thenReturn(cookies);
+        String result = CommonUtils.getCorrelationID(request);
+        assertEquals("testID", result);
+    }
+
+    @Test
+    public void testResolvespecifier() {
+        String token = "testToken";
+        HashMap ctx = new HashMap();
+        DocumentContext respCtx = Mockito.mock(DocumentContext.class);
+        Object req = new Object();
+        String result = CommonUtils.resolvespecifier(token, ctx, respCtx, req);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testSetTarget() {
+        String target = "$VAR.testTarget";
+        HashMap varmap = new HashMap();
+        DocumentContext dc = Mockito.mock(DocumentContext.class);
+        String value = "testValue";
+        boolean result = CommonUtils.setTarget(target, varmap, dc, value);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testResolveSource() {
+        String value = "$VAR.testValue";
+        HashMap varmap = new HashMap();
+        varmap.put("testValue", "resolvedValue");
+        Object req = new Object();
+        String result = CommonUtils.resolveSource(value, varmap, req);
+        assertEquals("resolvedValue", result);
+    }
+
+    @Test
+    public void testGetGlobalProp() {
+        String key = "java.version";
+        String result = CommonUtils.getGlobalProp(key);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testInitSystemProps() {
+        boolean result = CommonUtils.initSystemProps();
+        assertTrue(result);
+    }
+
+    @Test
+    public void testPrepareResponse() {
+        HashMap ctx = new HashMap();
+        DocumentContext dc = Mockito.mock(DocumentContext.class);
+        String rv = "testRv";
+        HashMap result = CommonUtils.prepareResponse(ctx, dc, rv);
+        assertNotNull(result);
+    }
+}
